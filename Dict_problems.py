2c20d68d584a2791e03772cad68ab48d4241bbb6
yaminimamidi <vijayamini@gmail.com>
2016-04-12 14:18:36 -0700

    Added files via upload

---
+++ Dict_problems.py
@@ -0,0 +1,19 @@
+months = {}
+months['one']    ="January"
+months['two']    ="February"
+months['three']  ="March"
+months['four']   ="April"
+months['five']   ="May"
+months['six']    ="June"
+months['seven']  ="July"
+months['eight']  ="August"
+months['nine']   ="September"
+months['ten']    ="October"
+months['eleven'] ="November"
+months['Twelve'] ="December"
+
+
+print months.keys()
+print months.values()
+print months.items()
+print months
